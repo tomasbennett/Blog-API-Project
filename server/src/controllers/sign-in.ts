e11e09e727ee8abc651750dc86eb784adaf4d078
tomasbennett <tjs.bennett@gmail.com>
2025-12-29 16:00:34 +0000

    Without ensuring that the user isn't already logged in we now have a login route that sends back both a refresh token and an access token as a cookie and as json respectively

--- server/src/controllers/sign-in.ts
+++ server/src/controllers/sign-in.ts
@@ -5,41 +5,21 @@ import passport from "passport";
 import { prisma } from "../db/prisma";
 
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken"
+import crypto from "crypto";
 
 import { ensureAuthentication, ensureNotAuthenticated } from "../passport/ensureAuthentication";
 import { ICustomSuccessMessage } from "../../../shared/features/api/models/APISuccessResponse";
-import { ISignInError, usernamePasswordSchema } from "../../../shared/features/auth/models/ILoginSchema";
+import { ILoginForm, ISignInError, usernamePasswordSchema } from "../../../shared/features/auth/models/ILoginSchema";
+import { environment } from "../../../shared/constants";
+import { IAccessTokenResponse } from "../../../shared/features/auth/models/IAccessTokenResponse";
 
 
 
 export const router = Router();
 
 
-router.post("/login", ensureNotAuthenticated, (req: Request, res: Response<ISignInError | { message: string, user: User }>, next: NextFunction) => {
-    passport.authenticate("local", (err: Error, user: User | false, info: ISignInError | undefined) => {
-        if (err) {
-            return next(err);
-        }
-        if (!user) {
-            return res.status(401).json({
-                message: info ? info.message : "Authentication failed",
-                inputType: info ? info.inputType : "root"
-            });
-
-        }
-        req.logIn(user, (err) => {
-            if (err) {
-                return next(err);
-            }
-
-            return res.status(200).json({ message: "Authentication successful", user });
-        });
-    })(req, res, next);
-
-});
-
-
-router.post("/register", ensureNotAuthenticated, async (req: Request<{}, {}, { username: string, password: string }>, res: Response<ISignInError | { message: string, user: User }>, next: NextFunction) => {
+router.post("/login", async (req: Request<{}, {}, ILoginForm>, res: Response<ISignInError | IAccessTokenResponse>, next: NextFunction) => {
     const { username, password } = req.body;
 
     const usernameResult = usernamePasswordSchema.safeParse(username);
@@ -61,23 +41,119 @@ router.post("/register", ensureNotAuthenticated, async (req: Request<{}, {}, { u
 
 
     try {
-        const hashedPassword: string = await bcrypt.hash(password, process.env.SALT_ROUNDS ? parseInt(process.env.SALT_ROUNDS) : 10);
+        const user: User | null = await prisma.user.findUnique({
+            where: {
+                username
+            }
+        });
 
-        const newUser = await prisma.user.create({
+        if (!user) {
+            const errorResponse: ISignInError = {
+                message: "Invalid username!!!",
+                inputType: "username"
+            };
+            return res.status(400).json(errorResponse);
+        }
+
+        const isPasswordValid: boolean = await bcrypt.compare(password, user.password);
+
+        if (!isPasswordValid) {
+            const errorResponse: ISignInError = {
+                message: "Invalid password!!!",
+                inputType: "password"
+            };
+            return res.status(400).json(errorResponse);
+        }
+
+        // WE WANT TO CREATE AN ACCESS TOKEN AND A REFRESH TOKEN FOR THE USER SENDING THE REFRESH TOKEN AS A COOKIE AND THE ACCESS TOKEN IN THE RESPONSE BODY
+        const accessToken = jwt.sign({
+            sub: user.id,
+            role: user.role
+        }, process.env.ACCESS_TOKEN_SECRET || "default_access_token_secret", {
+            expiresIn: '10m'
+        });
+
+        const refreshToken = crypto.randomBytes(64).toString('hex');
+
+        const refreshTokenHash = await bcrypt.hash(refreshToken, process.env.SALT_ROUNDS ? parseInt(process.env.SALT_ROUNDS) : 10);
+
+
+
+        const expiry: number = 7 * 24 * 60 * 60 * 1000;
+
+        await prisma.refreshToken.create({
             data: {
-                username,
-                password: hashedPassword
+                hashedToken: refreshTokenHash,
+                userId: user.id,
+                expiresAt: new Date(Date.now() + expiry)
             }
         });
 
-        req.logIn(newUser, (err) => {
-            if (err) {
-                return next(err);
-            }
+        res
+            .cookie("refreshToken", refreshToken, {
+                httpOnly: true,
+                secure: environment === "PROD",
+                sameSite: environment === "PROD" ? "none" : "lax",
+                maxAge: expiry
+            })
+            .status(200)
+            .json({
+                message: "Login successful",
+                ok: true,
+                status: 200,
+                accessToken
+            });
 
-            return res.status(201).json({ message: "User registered successfully", user: newUser });
+
+
+    } catch (error) {
+        return next(error);
+    }
+
+});
+
+
+router.post("/register", async (req: Request<{}, {}, ILoginForm>, res: Response<ISignInError | IAccessTokenResponse>, next: NextFunction) => {
+    const { username, password } = req.body;
+
+    const usernameResult = usernamePasswordSchema.safeParse(username);
+    if (!usernameResult.success) {
+        return res.status(400).json({
+            message: usernameResult.error.issues[0].message,
+            inputType: "username"
         });
 
+    }
+
+    const passwordResult = usernamePasswordSchema.safeParse(password);
+    if (!passwordResult.success) {
+        return res.status(400).json({
+            message: passwordResult.error.issues[0].message,
+            inputType: "password"
+        });
+    }
+
+
+    try {
+        // const hashedPassword: string = await bcrypt.hash(password, process.env.SALT_ROUNDS ? parseInt(process.env.SALT_ROUNDS) : 10);
+
+        // const newUser = await prisma.user.create({
+        //     data: {
+        //         username,
+        //         password: hashedPassword
+        //     }
+        // });
+
+        // req.logIn(newUser, (err) => {
+        //     if (err) {
+        //         return next(err);
+        //     }
+
+        //     return res.status(201).json({ message: "User registered successfully", user: newUser });
+        // });
+
+
+        
 
     } catch (error: unknown) {
         if (error instanceof Prisma.PrismaClientKnownRequestError) {
@@ -98,26 +174,9 @@ router.post("/register", ensureNotAuthenticated, async (req: Request<{}, {}, { u
 
 
 
-router.post("/logout", ensureAuthentication, (req: Request, res: Response<ICustomSuccessMessage>, next: NextFunction) => {
-
-    req.logOut(err => {
-        if (err) return next(err);
-
-        req.session.destroy((err) => {
-            if (err) return next(err);
+router.delete("/logout", ensureAuthentication, (req: Request, res: Response, next: NextFunction) => {
 
 
-            res.clearCookie("session-id");
-            return res.status(200).json({
-                ok: true,
-                status: 200,
-                message: "Successfully logged out!!!"
-            });
-
-        });
-
-
-    });
 });
 
 
